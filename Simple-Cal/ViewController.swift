d523927bd7c7a6e254387314cafa196f55b9e3f2
Matthew Chess <mchess@umich.edu>
2015-03-02 20:08:03 -0500

    Added operation cases

--- Simple-Cal/ViewController.swift
+++ Simple-Cal/ViewController.swift
@@ -10,6 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
+    
+    var userIsTypingANumber = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        if userIsTypingANumber{
+        display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsTypingANumber = true
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        if userIsTypingANumber {
+            enter()
+        }
+        switch operation{
+        case "×": preformOperation({ $0 * $1 })
+        case "÷": preformOperation({$1 / $0})
+        case "+": preformOperation({$0 + $1})
+        case "−": preformOperation({$1 - $0})
+        default: break
+        }
+    }
+    
+    func preformOperation(operation: (Double, Double) -> Double ){
+        
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+            }
+    }
+
+    
+    @IBAction func enter() {
+        userIsTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+        
+    }
+   
+    
+        var displayValue: Double{
+            get {
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }
+            set {
+                display.text = "\(newValue)"
+                userIsTypingANumber = false
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
